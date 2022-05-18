797422ba530d880e67cf00a315b0bde7f7b52a33
Igormcf <igormaciel82@gmail.com>
2022-05-18 19:27:47 -0300

    Adiciona favicon

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -4,7 +4,8 @@ import { connect } from 'react-redux';
 import { fetchCoins, fetchAddExchanges, editExpense } from '../actions';
 import TableExpenses from '../components/TableExpenses';
 import '../css/Wallet.css';
-import logo from '../images/logomywallet.png'
+import logo from '../images/logomywallet.png';
+import fav from '../images/favicon.ico';
 
 const alimentos = 'Alimentação';
 let idIndex = 0;
@@ -83,6 +84,7 @@ class Wallet extends React.Component {
   }
 
   render() {
+    <link rel='shortcut icon' href={ fav } type='image/x-icon'/>
     document.title = 'My Wallet';
     const { email, currencies, expenses, btnForm } = this.props;
     const { value, currency, description, method, tag, isButtonDisabled } = this.state;
