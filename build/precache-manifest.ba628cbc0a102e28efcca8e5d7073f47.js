8c5eaef796319e7b3dbef29ee57fa6176964ed51
Igormcf <igormaciel82@gmail.com>
2022-05-18 19:02:11 -0300

    modifica título da página

---
+++ build/precache-manifest.ba628cbc0a102e28efcca8e5d7073f47.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d768489036c23a4dd5d56520740945b",
+    "url": "/my-wallet/index.html"
+  },
+  {
+    "revision": "eae6b7c6fe3f328b4f0a",
+    "url": "/my-wallet/static/css/main.e6479281.chunk.css"
+  },
+  {
+    "revision": "93fbbc68bda0540424db",
+    "url": "/my-wallet/static/js/2.dcfd3666.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-wallet/static/js/2.dcfd3666.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eae6b7c6fe3f328b4f0a",
+    "url": "/my-wallet/static/js/main.36dedc42.chunk.js"
+  },
+  {
+    "revision": "da431a27d21fb4c134d8",
+    "url": "/my-wallet/static/js/runtime-main.faeefe17.js"
+  },
+  {
+    "revision": "883f6359a01ae59b0afe604750233335",
+    "url": "/my-wallet/static/media/img3.883f6359.png"
+  },
+  {
+    "revision": "8ad3333b9a33b148e8f33ede3e7b60d8",
+    "url": "/my-wallet/static/media/img4.8ad3333b.png"
+  },
+  {
+    "revision": "da84bafdb8b46eb13b9480b8b7322dbc",
+    "url": "/my-wallet/static/media/logomywallet.da84bafd.png"
+  }
+]);
