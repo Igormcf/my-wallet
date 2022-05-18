be779135b6222b21a6e7c99168fea5d60f55c1bc
Igormcf <igormaciel82@gmail.com>
2022-05-18 18:28:46 -0300

    Add build

---
+++ build/precache-manifest.8741a2119e4c81202f16b73a235a4000.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fd08c4e9e868d8b332ab6e5dc90d3ea",
+    "url": "/app-toot-name/index.html"
+  },
+  {
+    "revision": "c33ceb1082c76bf93428",
+    "url": "/app-toot-name/static/css/main.0dd034c7.chunk.css"
+  },
+  {
+    "revision": "5fc1799f39891941c032",
+    "url": "/app-toot-name/static/js/2.64c4a60b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/app-toot-name/static/js/2.64c4a60b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c33ceb1082c76bf93428",
+    "url": "/app-toot-name/static/js/main.2236bc4e.chunk.js"
+  },
+  {
+    "revision": "66ac581fbc8990623b1d",
+    "url": "/app-toot-name/static/js/runtime-main.cfc30c8d.js"
+  },
+  {
+    "revision": "883f6359a01ae59b0afe604750233335",
+    "url": "/app-toot-name/static/media/img3.883f6359.png"
+  },
+  {
+    "revision": "8ad3333b9a33b148e8f33ede3e7b60d8",
+    "url": "/app-toot-name/static/media/img4.8ad3333b.png"
+  },
+  {
+    "revision": "da84bafdb8b46eb13b9480b8b7322dbc",
+    "url": "/app-toot-name/static/media/logomywallet.da84bafd.png"
+  }
+]);
